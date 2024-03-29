00752496c095ea39a011e56d8fef061dfccb82ad
karthikchary2003 <133854727+karthikchary2003@users.noreply.github.com>
2024-03-29 22:39:16 +1000

    Update script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+
+gsap.from("a,#nav,#right-nav",{
+    y:-200,
+    opacity:0,
+    stagger:1
+
+})
+gsap.from("#textcenter",{
+    x:-400,
+    delay:1,
+    duration:0.5,
+    opacity:0,
+    stagger:1
+
+});
